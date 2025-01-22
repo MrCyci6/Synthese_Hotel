328c9f77ccfd2f2fb06a40b5a96c8e77d6760381
Cyriac LENOIR <cyriac.lenoir@isen-ouest.yncrea.fr>
2025-01-22 15:54:19 +0100

    feat(front): dashboard stats front

--- admin/panel.php
+++ admin/panel.php
@@ -26,8 +26,8 @@
     <div class="sidebar d-flex flex-column border-end">
         <!-- Header -->
         <div class="container border-bottom d-flex justify-content-center align-items-center">
-            <svg class="me-3" style="color: rgb(37 99 235);" xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-hotel h-8 w-8 text-blue-600"><path d="M18 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2Z"></path><path d="m9 16 .348-.24c1.465-1.013 3.84-1.013 5.304 0L15 16"></path><path d="M8 7h.01"></path><path d="M16 7h.01"></path><path d="M12 7h.01"></path><path d="M12 11h.01"></path><path d="M16 11h.01"></path><path d="M8 11h.01"></path><path d="M10 22v-6.5m4 0V22"></path></svg>
-            <h4 class="text-center py-3">Au-Tel-2-Lux</h4>
+            <svg class="me-2" style="color: rgb(37 99 235);" xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-hotel h-8 w-8 text-blue-600"><path d="M18 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2Z"></path><path d="m9 16 .348-.24c1.465-1.013 3.84-1.013 5.304 0L15 16"></path><path d="M8 7h.01"></path><path d="M16 7h.01"></path><path d="M12 7h.01"></path><path d="M12 11h.01"></path><path d="M16 11h.01"></path><path d="M8 11h.01"></path><path d="M10 22v-6.5m4 0V22"></path></svg>
+            <h4 class="text-center py-3 fw-bold">Au-Tel-2-Lux</h4>
         </div>
         <!-- Gestion -->
         <div class="container border-bottom p-2">
@@ -36,7 +36,7 @@
                 <h6 class="ms-3 mt-1">Gestion permissions</h6>
             </a>
 
-            <select id="selectHotel" class="form-select mb-1">
+            <select id="selectHotel" class="form-select mb-1 mt-1">
                 <option selected>Sélectionner un hotel</option>
                 <option value="1">Hotel de Caen</option>
                 <option value="2">Hotel de Brest</option>
@@ -74,17 +74,74 @@
         <div class="topbar d-flex align-items-center justify-content-between px-4">
             <div class="container d-flex flex-column">    
                 <h5 class="mb-0">Hotel de caen</h5>
-                <span class="">Standard</span>
+                <span class="text-secondary">Standard</span>
             </div>    
             <div class="container d-flex justify-content-end">    
                 <span class="me-2">Super Admin</span>
-                <a class="logout.php"><svg style="color: none;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-out h-5 w-5"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" x2="9" y1="12" y2="12"></line></svg></a>
+                <a class="logout.php"><svg style="color: rgb(75, 85, 99);" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-out h-5 w-5"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" x2="9" y1="12" y2="12"></line></svg></a>
             </div>  
         </div>
 
         <!-- Content -->
-        <div class="p-4">
-            <!-- TODO -->
+        <div class="bg-body-tertiary p-4">
+            <div class="row g-4">
+                <!-- Booking -->
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="card p-3 rounded-xl border-0">
+                        <div class="d-flex flex-row align-items-center justify-content-around">
+                            <div class="d-flex flex-column justify-content-center">
+                                <span class="fs-6 text-secondary">Réservations</span>
+                                <p class="fs-4 fw-semibold">10</p>
+                            </div>
+                            <div class="bg-primary-subtle rounded p-2 me-3 d-flex align-items-center justify-content-center" style="width: 50px; height: 50px;">
+                                <svg style="color: rgb(37, 99, 235);" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar h-6 w-6 text-blue-600"><path d="M8 2v4"></path><path d="M16 2v4"></path><rect width="18" height="18" x="3" y="4" rx="2"></rect><path d="M3 10h18"></path></svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Revenue -->
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="card p-3 rounded-xl border-0">
+                        <div class="d-flex flex-row align-items-center justify-content-around">
+                            <div class="d-flex flex-column justify-content-center">
+                                <span class="fs-6 text-secondary">Revenus</span>
+                                <p class="fs-4 fw-semibold">14789€</p>
+                            </div>
+                            <div class="bg-success-subtle rounded p-2 me-3 d-flex align-items-center justify-content-center" style="width: 50px; height: 50px;">
+                                <svg style="color: rgb(22, 163, 74);" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-dollar-sign h-6 w-6 text-green-600"><line x1="12" x2="12" y1="2" y2="22"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Occupation rate -->
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="card p-3 rounded-xl border-0">
+                        <div class="d-flex flex-row align-items-center justify-content-around">
+                            <div class="d-flex flex-column justify-content-center">
+                                <span class="fs-6 text-secondary">Occupation</span>
+                                <p class="fs-4 fw-semibold">30%</p>
+                            </div>
+                            <div class=" rounded p-2 me-3 d-flex align-items-center justify-content-center" style="width: 50px; height: 50px; background-color: rgba(147, 51, 234, 0.2)">
+                                <svg style="color: rgb(147, 51, 234);" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-users h-6 w-6 text-purple-600"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M22 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Daily average revenue -->
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="card p-3 rounded-xl border-0">
+                        <div class="d-flex flex-row align-items-center justify-content-around">
+                            <div class="d-flex flex-column justify-content-center">
+                                <span class="fs-6 text-secondary">Moy. jour</span>
+                                <p class="fs-4 fw-semibold">137€</p>
+                            </div>
+                            <div class="bg-warning-subtle rounded p-2 me-3 d-flex align-items-center justify-content-center" style="width: 50px; height: 50px;">
+                                <svg style="color: rgb(234, 88, 12);" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trending-up h-6 w-6 text-orange-600"><polyline points="22 7 13.5 15.5 8.5 10.5 2 17"></polyline><polyline points="16 7 22 7 22 13"></polyline></svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
