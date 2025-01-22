20e052d14e8221400aa8512830f613cc4a7f1a9f
Cyriac LENOIR <cyriac.lenoir@isen-ouest.yncrea.fr>
2025-01-22 23:01:07 +0100

    fix(dashboard): perm module only for super admin

--- admin/panel.php
+++ admin/panel.php
@@ -79,10 +79,13 @@
 
         <!-- Gestion -->
         <div class="container border-bottom p-2">
-            <a class="d-flex align-items-center mb-2 justify-content-center" style="border-radius: 15px;">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shield h-5 w-5"><path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z"></path></svg>
-                <h6 class="ms-3 mt-1">Gestion permissions</h6>
-            </a>
+            <?php 
+                if($userId==ADMIN_ID)
+                    echo "<a class=\"d-flex align-items-center mb-2 justify-content-center\" style=\"border-radius: 15px;\">
+                        <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"lucide lucide-shield h-5 w-5\"><path d=\"M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z\"></path></svg>
+                        <h6 class=\"ms-3 mt-1\">Gestion permissions</h6>
+                    </a>";
+            ?>
 
             <!-- Hotel list -->
             <select id="selectHotel" class="form-select mb-1 mt-1">
@@ -137,7 +140,7 @@
                 <span class="text-secondary"><?php echo $hotelClasse ?></span>
             </div>    
             <div class="container d-flex justify-content-end">    
-                <span class="me-2">Super Admin</span>
+                <span class="me-2">TODO nom/prenom</span>
                 <a href="logout.php"><svg style="color: rgb(75, 85, 99);" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-out h-5 w-5"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" x2="9" y1="12" y2="12"></line></svg></a>
             </div>  
         </div>
