3fe9b7997f06916fab7d3c822b308e1dbf89c204
mbross28 <mathis.brosseau@isen-ouest.yncrea.fr>
2025-01-28 16:16:33 +0100

    config database

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,6 +1,6 @@
 <?php
-    const DB_USERNAME = "hotel_admin";
-    const DB_PASSWORD = "hotel";
+    const DB_USERNAME = "hotel";
+    const DB_PASSWORD = "isen";
     const DB_NAME = "hotel";
     const DB_HOST = "127.0.0.1";
     const DB_PORT = 5432;
