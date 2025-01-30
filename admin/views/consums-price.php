fd36fd8c2dc98672bfc0be44b2e2cc3a4879ea89
mbross28 <mathis.brosseau@isen-ouest.yncrea.fr>
2025-01-30 15:37:05 +0100

    ajustement position carrousel

--- admin/views/consums-price.php
+++ admin/views/consums-price.php
@@ -13,22 +13,10 @@
 
 </head>
 <body>
-<div>
-    <div class="container border-bottom ">
+<div  class="container text-center">
+    <div class="container border-bottom">
         <h1 class="text-center py-3 fw-bold">Menu de nos consommations</h1>
     </div>
-
-    <div>
-        <h4 class="text-center py-3 fw-bold">Voici la liste de nos consommations :</h4>
-    </div>
-
-
-
-
-
-
-
-
     <div class="table-responsive">
         <table class="table table-dark table-striped">
             <thead>
@@ -48,26 +36,33 @@
             </tbody>
         </table>
     </div>
-</div>
-<div id="carouselExample" class="carousel slide">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="images/img1.jpg" class="d-block w-100" alt="bs">
-        </div>
-        <div class="carousel-item">
-            <img src="images/img2.jpg" class="d-block w-100" alt="pt">
-        </div>
+    <div class="row">
+        <div class="col-5 col-md-3"></div>
+        <div class="col-8 col-md-6">
+            <div id="carouselExample" class="carousel slide " >
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="images/img1.jpg" class="d-block w-100" alt="bs">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/img2.jpg" class="d-block w-100" alt="pt">
+                    </div>
 
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+        <div class="col-5 col-md-3"></div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
 </div>
+
 
 <script src="./js/panel-dropdown.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
