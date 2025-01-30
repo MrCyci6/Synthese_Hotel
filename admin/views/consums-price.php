3a4d2df701d081696650746139934fa7116724c9
mbross28 <mathis.brosseau@isen-ouest.yncrea.fr>
2025-01-30 15:53:40 +0100

    suppression carrousel et ajout bouton

--- admin/views/consums-price.php
+++ admin/views/consums-price.php
@@ -13,58 +13,37 @@
 
 </head>
 <body>
-<div  class="container text-center">
-    <div class="container border-bottom">
-        <h1 class="text-center py-3 fw-bold">Menu de nos consommations</h1>
-    </div>
-    <div class="table-responsive">
-        <table class="table table-dark table-striped">
-            <thead>
-            <tr>
-                <th>Dénomination</th>
-                <th>Prix</th>
-            </tr>
-            </thead>
-            <tbody>
-
-            <?php
-            for ($i = 0; $i <= $nbConso; $i++) {
-                echo "<tr><td>".$consommations[$i]['denomination']."</td><td>".$consommations[$i]['prix']."</td></td></tr>";
-            }
-            ?>
-
-            </tbody>
-        </table>
+<div class="container ">
+    <div class="row">
+        <div class="col-md-8">
+            <h1 class="py-3 fw-bold">Menu de nos consommations</h1>
+        </div>
+        <div class="col-6 col-md-4">
+            <button type="button" class="btn btn-primary">Éditer prix</button>
+        </div>
     </div>
     <div class="row">
-        <div class="col-5 col-md-3"></div>
-        <div class="col-8 col-md-6">
-            <div id="carouselExample" class="carousel slide " >
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="images/img1.jpg" class="d-block w-100" alt="bs">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="images/img2.jpg" class="d-block w-100" alt="pt">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="images/img3.jpg" class="d-block w-100" alt="pt">
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
+        <div class="table-responsive">
+            <table class="table table-white table-striped">
+                <thead>
+                <tr>
+                    <th>Dénomination</th>
+                    <th>Prix</th>
+                </tr>
+                </thead>
+                <tbody>
+
+                <?php
+                for ($i = 0; $i <= $nbConso; $i++) {
+                    echo "<tr><td>".$consommations[$i]['denomination']."</td><td>".$consommations[$i]['prix']."</td></td></tr>";
+                }
+                ?>
+
+                </tbody>
+            </table>
         </div>
-        <div class="col-5 col-md-3"></div>
     </div>
 </div>
-
 
 <script src="./js/panel-dropdown.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
