15c96df1975b1ccb72abbb83a72da4e69ecd5bbd
mbross28 <mathis.brosseau@isen-ouest.yncrea.fr>
2025-01-30 15:39:46 +0100

    ajout photo carrousel

--- admin/views/consums-price.php
+++ admin/views/consums-price.php
@@ -47,7 +47,9 @@
                     <div class="carousel-item">
                         <img src="images/img2.jpg" class="d-block w-100" alt="pt">
                     </div>
-
+                    <div class="carousel-item">
+                        <img src="images/img3.jpg" class="d-block w-100" alt="pt">
+                    </div>
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
