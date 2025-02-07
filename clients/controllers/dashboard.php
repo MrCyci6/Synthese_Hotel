428fa6d6441cd37ec3f1df1d8ca0ee99881c49c4
Dacks <thomas.geffroy@outlook.com>
2025-02-07 09:59:14 +0100

    Add dashboard view with basic HTML structure and styling

--- clients/controllers/dashboard.php
+++ clients/controllers/dashboard.php
@@ -1,2 +1,4 @@
 <?php
 require_once __DIR__ . '/../models/Client.php';
+
+require __DIR__ . '/../views/dashboard.php';
--- clients/views/dashboard.php
+++ clients/views/dashboard.php
@@ -1,1 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+		<title>Dashboard</title>
+	</head>
+	<body>
+	</body>
+	<body>
+	<div class="topbar d-flex  position-relative">
+		<div class="left-0 top-0 position-absolute bg-white col-xxl-12"></div>
+		<div class="position-absolute col-xxl-11 row justify-content-between align-items-center">
+			<div class="col-xl-1 row justify-content-between align-items-center d-flex">
+				<div class="position-relative col-2"></div>
+				<p class="text-black fs-6 fw-normal font-family-Roboto Flex col-9 m-0 px-3 py-2">Au Tel 2 Lux</p>
+			</div>
+			<div class="px-1 col-xl-3 row justify-content-center align-items-center d-flex">
+				<p class="text-black fs-6 fw-normal font-family-Roboto Flex col-2 m-0 px-3 py-2">Home</p>
+				<p class="text-black fs-6 fw-normal font-family-Roboto Flex col-2 m-0 px-3 py-2">Support</p>
+				<p class="text-black fs-6 fw-normal font-family-Roboto Flex col-3 m-0 px-3 py-2">My Account</p>
+				<div class="position-relative col-1"></div>
+			</div>
+		</div>
+	</div>
+	</body>
+</html>
