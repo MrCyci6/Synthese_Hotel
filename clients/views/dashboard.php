2744e93e18cdac604de97fba120ff22321765fab
Dacks <114010431+Dackss@users.noreply.github.com>
2025-02-10 20:34:00 +0100

    feat(username): update dashboard layout and styling 2 with fixed topbar

--- clients/views/dashboard.php
+++ clients/views/dashboard.php
@@ -7,25 +7,35 @@
 		<title>Dashboard Client</title>
 		<link rel="stylesheet" href="../styles/dashboard.css">
 	</head>
-
-	<body>
-		<div class="topbar d-flex justify-content-between align-items-center">
-			<div class="container d-flex align-items-center flex-shrink-0 justify-content-between" style="width: 100%;">
+	<body class="body">
+		<!-- TopBar -->
+		<div class="topbar d-flex align-items-center">
+			<div class="container-fluid d-flex justify-content-between align-items-center">
 				<!-- Home Link -->
 				<a href="" class="text-decoration-none d-flex align-items-center home-link">
 					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" class="home-link-icon">
 						<path d="M7 14C8.66 14 10 12.66 10 11C10 9.34 8.66 8 7 8C5.34 8 4 9.34 4 11C4 12.66 5.34 14 7 14ZM7 10C7.55 10 8 10.45 8 11C8 11.55 7.55 12 7 12C6.45 12 6 11.55 6 11C6 10.45 6.45 10 7 10ZM19 7H11V15H3V5H1V20H3V17H21V20H23V11C23 8.79 21.21 7 19 7ZM21 15H13V9H19C20.1 9 21 9.9 21 11V15Z" fill="black"/>
 					</svg>
-					<span class="home-link-text">Au-tel-2-Lux</span>
+					<span class="home-link-text ms-2">Au-tel-2-Lux</span>
 				</a>
 
 				<!-- Links -->
-				<div class="d-flex justify-content-center align-items-center links">
+				<div class="d-flex gap-3 links">
 					<a href="" class="text-decoration-none text-dark">Home</a>
-					<a href="" class="text-decoration-none text-dark">Home</a>
-					<a href="" class="text-decoration-none text-dark">Home</a>
+					<a href="" class="text-decoration-none text-dark">Support</a>
+					<a href="" class="text-decoration-none text-dark">My Account</a>
+					<a href="" class="text-decoration-none text-dark">
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+							<path d="M5 21C4.45 21 3.97933 20.8043 3.588 20.413C3.19667 20.0217 3.00067 19.5507 3 19V5C3 4.45 3.196 3.97933 3.588 3.588C3.98 3.19667 4.45067 3.00067 5 3H12V5H5V19H12V21H5ZM16 17L14.625 15.55L17.175 13H9V11H17.175L14.625 8.45L16 7L21 12L16 17Z" fill="black"/>
+						</svg>
+					</a>
 				</div>
 			</div>
+		</div>
+
+		<!-- Main Section -->
+		<div class="main-section">
+
 		</div>
 	</body>
 </html>
