717625396a3939c09f9a797fdf0d24e47372ccbc
Dacks <114010431+Dackss@users.noreply.github.com>
2025-02-10 20:03:37 +0100

    feat(dacks): refine dashboard layout and styling
    Updated the dashboard's HTML and CSS for improved structure and design, integrating a new topbar and link styling.

--- clients/views/dashboard.php
+++ clients/views/dashboard.php
@@ -4,25 +4,28 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-		<title>Dashboard</title>
+		<title>Dashboard Client</title>
+		<link rel="stylesheet" href="../styles/dashboard.css">
 	</head>
+
 	<body>
-	</body>
-	<body>
-	<div class="topbar d-flex  position-relative">
-		<div class="left-0 top-0 position-absolute bg-white col-xxl-12"></div>
-		<div class="position-absolute col-xxl-11 row justify-content-between align-items-center">
-			<div class="col-xl-1 row justify-content-between align-items-center d-flex">
-				<div class="position-relative col-2"></div>
-				<p class="text-black fs-6 fw-normal font-family-Roboto Flex col-9 m-0 px-3 py-2">Au Tel 2 Lux</p>
-			</div>
-			<div class="px-1 col-xl-3 row justify-content-center align-items-center d-flex">
-				<p class="text-black fs-6 fw-normal font-family-Roboto Flex col-2 m-0 px-3 py-2">Home</p>
-				<p class="text-black fs-6 fw-normal font-family-Roboto Flex col-2 m-0 px-3 py-2">Support</p>
-				<p class="text-black fs-6 fw-normal font-family-Roboto Flex col-3 m-0 px-3 py-2">My Account</p>
-				<div class="position-relative col-1"></div>
+		<div class="topbar d-flex justify-content-between align-items-center">
+			<div class="container d-flex align-items-center flex-shrink-0 justify-content-between" style="width: 100%;">
+				<!-- Home Link -->
+				<a href="" class="text-decoration-none d-flex align-items-center home-link">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" class="home-link-icon">
+						<path d="M7 14C8.66 14 10 12.66 10 11C10 9.34 8.66 8 7 8C5.34 8 4 9.34 4 11C4 12.66 5.34 14 7 14ZM7 10C7.55 10 8 10.45 8 11C8 11.55 7.55 12 7 12C6.45 12 6 11.55 6 11C6 10.45 6.45 10 7 10ZM19 7H11V15H3V5H1V20H3V17H21V20H23V11C23 8.79 21.21 7 19 7ZM21 15H13V9H19C20.1 9 21 9.9 21 11V15Z" fill="black"/>
+					</svg>
+					<span class="home-link-text">Au-tel-2-Lux</span>
+				</a>
+
+				<!-- Links -->
+				<div class="d-flex justify-content-center align-items-center links">
+					<a href="" class="text-decoration-none text-dark">Home</a>
+					<a href="" class="text-decoration-none text-dark">Home</a>
+					<a href="" class="text-decoration-none text-dark">Home</a>
+				</div>
 			</div>
 		</div>
-	</div>
 	</body>
 </html>
