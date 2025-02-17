3ff43c05e307ffd9b82a83bb0e41a66a55659e4d
Dacks <114010431+Dackss@users.noreply.github.com>
2025-02-17 13:04:59 +0100

    feat(username): Update dashboard UI with SVG icons and improved spacing

--- clients/views/dashboard.php
+++ clients/views/dashboard.php
@@ -36,7 +36,12 @@
 					<!-- Barre de navigation -->
 					<nav class="navbar navbar-expand-lg navbar-light bg-white px-3">
 						<div class="container-fluid">
-							<a class="navbar-brand" href="#">Au Tel 2 Lux</a>
+							<a class="navbar-brand" href="#">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+									<path d="M7 14C8.66 14 10 12.66 10 11C10 9.34 8.66 8 7 8C5.34 8 4 9.34 4 11C4 12.66 5.34 14 7 14ZM7 10C7.55 10 8 10.45 8 11C8 11.55 7.55 12 7 12C6.45 12 6 11.55 6 11C6 10.45 6.45 10 7 10ZM19 7H11V15H3V5H1V20H3V17H21V20H23V11C23 8.79 21.21 7 19 7ZM21 15H13V9H19C20.1 9 21 9.9 21 11V15Z" fill="black"/>
+								</svg>
+								Au Tel 2 Lux
+							</a>
 							<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
 								<ul class="navbar-nav ms-auto">
 									<li class="nav-item">
@@ -58,7 +63,12 @@
 							</button>
 							<div class="offcanvas offcanvas-start d-lg-none" tabindex="-1" id="sidebarOffcanvas" aria-labelledby="sidebarOffcanvasLabel">
 								<div class="offcanvas-header">
-									<h5 class="offcanvas-title" id="sidebarOffcanvasLabel">Au Tel 2 Lux</h5>
+									<h5 class="offcanvas-title" id="sidebarOffcanvasLabel">
+										<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+											<path d="M7 14C8.66 14 10 12.66 10 11C10 9.34 8.66 8 7 8C5.34 8 4 9.34 4 11C4 12.66 5.34 14 7 14ZM7 10C7.55 10 8 10.45 8 11C8 11.55 7.55 12 7 12C6.45 12 6 11.55 6 11C6 10.45 6.45 10 7 10ZM19 7H11V15H3V5H1V20H3V17H21V20H23V11C23 8.79 21.21 7 19 7ZM21 15H13V9H19C20.1 9 21 9.9 21 11V15Z" fill="black"/>
+										</svg>
+										Au Tel 2 Lux
+									</h5>
 									<button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
 								</div>
 								<div class="offcanvas-body d-flex flex-column align-items-center justify-content-center p-0">
@@ -73,32 +83,32 @@
 											<a class="nav-link" href="#">My Account</a>
 										</li>
 										<li class="nav-item">
-											<a class="nav-link" href="#"><i class="bi bi-box-arrow-up-right"></i></a>
+											<a class="nav-link" href="#"><i class="bi bi-box-arrow-up-right"></i> Déconnexion</a>
 										</li>
 										<li class="nav-item">
-											<a href="#" class="nav-link"><i class="bi bi-layout-text-sidebar-reverse"></i></a>
+											<a href="#" class="nav-link"><i class="bi bi-layout-text-sidebar-reverse"></i> Tableau de Bord</a>
 										</li>
 										<li class="nav-item">
-											<a href="#" class="nav-link"><i class="bi bi-heart"></i></a>
+											<a href="#" class="nav-link"><i class="bi bi-heart"></i> Réservations Favorites</a>
 										</li>
 										<li class="nav-item">
-											<a href="#" class="nav-link"><i class="bi bi-bell"></i></a>
+											<a href="#" class="nav-link"><i class="bi bi-bell"></i> Notifications</a>
 										</li>
 										<li class="nav-item">
-											<a href="#" class="nav-link"><i class="bi bi-cash-stack"></i></a>
+											<a href="#" class="nav-link"><i class="bi bi-cash-stack"></i> Moyen de Réglements</a>
 										</li>
 										<li class="nav-item">
-											<a href="#" class="nav-link"><i class="bi bi-gear"></i></a>
+											<a href="#" class="nav-link"><i class="bi bi-gear"></i> Paramètres</a>
 										</li>
 									</ul>
 								</div>
 							</div>
 						</div>
 					</nav>
-					<div class="main-section flex-fill p-3 m-5">
+					<div class="main-section flex-fill p-3 m-3">
 						<div class="row g-3">
 							<div class="col-md-3">
-								<div class="card p-2 bg-white border rounded-3">
+								<div class="card p-3 bg-white border rounded-3">
 									<div class="card-body">
 										<h5 class="card-title">Taux d'occupation</h5>
 										<p class="card-text">10%</p>
@@ -106,7 +116,7 @@
 								</div>
 							</div>
 							<div class="col-md-3">
-								<div class="card p-2 bg-red text-white rounded-3">
+								<div class="card p-3 bg-red text-white rounded-3">
 									<div class="card-body">
 										<h5 class="card-title">Total des consommations</h5>
 										<p class="card-text">25€</p>
@@ -114,7 +124,7 @@
 								</div>
 							</div>
 							<div class="col-md-3">
-								<div class="card p-2 bg-yellow text-white rounded-3">
+								<div class="card p-3 bg-yellow text-white rounded-3">
 									<div class="card-body">
 										<h5 class="card-title">Nombre de plaintes reçues</h5>
 										<p class="card-text">0</p>
@@ -122,7 +132,7 @@
 								</div>
 							</div>
 							<div class="col-md-3">
-								<div class="card p-2 bg-orange text-white rounded-3">
+								<div class="card p-3 bg-orange text-white rounded-3">
 									<div class="card-body">
 										<h5 class="card-title">Date de départ</h5>
 										<p class="card-text">25/12/25</p>
