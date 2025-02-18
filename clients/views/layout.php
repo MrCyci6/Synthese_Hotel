14aec0d71e3f1b6a3545ddb539123fa2ae9829bc
Dacks <114010431+Dackss@users.noreply.github.com>
2025-02-18 17:31:49 +0100

    add(dacks): Introduce modular layout with header, navbar, sidebar, and dashboard
    Added new partials (`header.php`, `navbar.php`, `sidebar.php`) and updated the main layout for modular structure. Simplified `dashboard.php` by refactoring components into separate files.

---
+++ clients/views/layout.php
@@ -0,0 +1,9 @@
+<?php
+
+$pageTitle = $pageTitle ?? 'Erreur Titre';
+
+include 'partials/header.php';
+include 'partials/sidebar.php';
+include 'partials/navbar.php';
+
+	include 'dashboard.php';
---
+++ clients/views/partials/sidebar.php
@@ -0,0 +1,22 @@
+<div class="container-fluid" style="overflow-x: hidden" >
+	<div class="row flex-nowrap justify-content-center">
+		<!-- Barre latérale -->
+		<nav class="col-auto sidebar d-none d-lg-flex flex-column align-items-center justify-content-center min-vh-100">
+			<ul class="nav flex-column p-0 m-0">
+				<li class="nav-item">
+					<a href="#" class="nav-link active"><i class="bi bi-layout-text-sidebar-reverse"></i></a>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link"><i class="bi bi-heart"></i></a>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link"><i class="bi bi-bell"></i></a>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link"><i class="bi bi-cash-stack"></i></a>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link"><i class="bi bi-gear"></i></a>
+				</li>
+			</ul>
+		</nav>
