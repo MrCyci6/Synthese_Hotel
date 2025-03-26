145218865f4bb529a30d6d2ef28b8a45fb323c52
Cyriac LENOIR <cyriac.lenoir@isen-ouest.yncrea.fr>
2025-03-26 17:40:30 +0100

    fix(dashboard): resize box

--- admin/views/consums/consums-price.php
+++ admin/views/consums/consums-price.php
@@ -1,3 +1,5 @@
+<div class="p-4 mt-3">
+    <div class="container">
         <div class="row">
             <div class="col-md-8">
                 <h1 class="py-3 fw-bold">Menu de nos consommations</h1>
@@ -79,6 +81,7 @@
         </div>
     </div>
 </div>
+
 <script src="./js/panel-dropdown.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
--- admin/views/consums/consums.php
+++ admin/views/consums/consums.php
@@ -1,3 +1,5 @@
+<div class="p-4 mt-3">
+    <div class="container">
         <div class="row">
             <div class="col-md-8">
                 <h1 class="py-3 fw-bold">Récapitulatif consommations</h1>
@@ -28,6 +30,7 @@
         </div>
     </div>
 </div>
+
 <script src="./js/panel-dropdown.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
