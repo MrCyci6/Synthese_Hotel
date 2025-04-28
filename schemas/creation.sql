b947da7ab8110b62d2bdae361a5f75701b1c8e0c
Cyriac LENOIR <cyriac.lenoir@isen-ouest.yncrea.fr>
2025-04-28 08:37:17 +0200

    feat(client): Fix past reservation

--- schemas/creation.sql
+++ schemas/creation.sql
@@ -29,6 +29,7 @@ create table Hotel
 	id_hotel SERIAL,
 	nom varchar(50) not null,
 	id_classe int not null,
+	localisation varchar(50) not null
 PRIMARY KEY(id_hotel),
 FOREIGN KEY (id_classe) REFERENCES Classe (id_classe)
 );
@@ -130,4 +131,13 @@ CREATE TABLE Logs
 PRIMARY KEY (id_log),
 FOREIGN KEY (id_hotel) REFERENCES Hotel(id_hotel),
 FOREIGN KEY (id_user) REFERENCES Users(id_user) ON DELETE CASCADE
+);
+
+create table Services
+(
+    id_service  serial
+        primary key,
+    nom         varchar(100) not null,
+    description text         not null,
+    image_url   varchar(255) default NULL::character varying
 );
